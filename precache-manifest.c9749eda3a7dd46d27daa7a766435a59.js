90186e7a58409a6ec908e6a17508fe4719245f94
Aniket Kumar <aniketskr@gmail.com>
2022-07-18 19:39:04 +0530

    Updates

---
+++ precache-manifest.c9749eda3a7dd46d27daa7a766435a59.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e482871168804167700c3bd3fba6636",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "f9da775e41ab7372cddc",
+    "url": "/Keeper-App/static/js/2.398dbefe.chunk.js"
+  },
+  {
+    "revision": "59666612a6e3f1081375",
+    "url": "/Keeper-App/static/js/main.4e8f072e.chunk.js"
+  },
+  {
+    "revision": "4997685bab9cff726bba",
+    "url": "/Keeper-App/static/js/runtime-main.31b25eb3.js"
+  }
+]);
